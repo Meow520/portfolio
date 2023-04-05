37eaec9191792248c364431603e9683638a00de0
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2023-04-05 11:44:13 +0900

    fix api #2

--- pages/api/news-api/[id].ts
+++ pages/api/news-api/[id].ts
@@ -6,50 +6,51 @@ const prisma = new PrismaClient();
 
 // eslint-disable-next-line import/no-anonymous-default-export
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.method) {
-    case "GET":
-      return await getNews(req, res);
-    case "PUT":
-      return await updateNews(req, res);
-    case "DELETE":
-      return await deleteNews(req, res);
-  }
+  try {
+        const news = await prisma.news.findFirst({
+          where: { id: Number(req.query.id) },
+        });
+        return res.status(200).json(news);
+      } catch (error: unknown) {
+        if (error instanceof Error)
+          return res.status(500).json({ message: error.message });
+      }
 };
 
 export default handler;
 
-const getNews = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const news = await prisma.news.findUnique({ where: { id: req.query.id } });
-    const news: newsData = results[0];
-    return res.status(200).json(news);
-  } catch (error: unknown) {
-    if (error instanceof Error)
-      return res.status(500).json({ message: error.message });
-  }
-};
+// const getNews = async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const news = await prisma.news.findFirst({
+//       where: { id: Number(req.query.id) },
+//     });
+//     return res.status(200).json(news);
+//   } catch (error: unknown) {
+//     if (error instanceof Error)
+//       return res.status(500).json({ message: error.message });
+//   }
+// };
 
-const updateNews = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const results = await mysql.query("UPDATE news SET ? WHERE id = ?", [
-      req.body,
-      req.query.id,
-    ]);
-    const news: newsData = results[0];
-    return res.status(200).json(news);
-  } catch (error: unknown) {
-    if (error instanceof Error)
-      return res.status(500).json({ message: error.message });
-  }
-};
-const deleteNews = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const results = await mysql.query("DELETE FROM news WHERE id = ?", [
-      req.query.id,
-    ]);
-    return res.status(204).json({ message: "deleted" });
-  } catch (error: unknown) {
-    if (error instanceof Error)
-      return res.status(500).json({ message: error.message });
-  }
-};
+// const updateNews = async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const news = await prisma.news.update({
+//       where: { id: Number(req.query.id) },
+//       data: req.body,
+//     });
+//     return res.status(200).json(news);
+//   } catch (error: unknown) {
+//     if (error instanceof Error)
+//       return res.status(500).json({ message: error.message });
+//   }
+// };
+// const deleteNews = async (req: NextApiRequest, res: NextApiResponse) => {
+//   try {
+//     const results = await prisma.news.delete({
+//       where: { id: Number(req.query.id) },
+//     });
+//     return res.status(204).json({ message: "deleted" });
+//   } catch (error: unknown) {
+//     if (error instanceof Error)
+//       return res.status(500).json({ message: error.message });
+//   }
+// };
