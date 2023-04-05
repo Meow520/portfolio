ff35efd14ebcb4f261616695b241683820435680
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2023-04-05 12:06:32 +0900

    fix api #3

--- pages/api/news-api.ts
+++ pages/api/news-api.ts
@@ -1,6 +1,7 @@
 import { News } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<News[]>) => {
