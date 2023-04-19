8da2a8c7ae6ae6e0a99257a63ffe50132c192bd3
Mao Yamaguchi <maomao.xmdhkdltm@gmail.com>
2023-04-19 22:25:29 +0900

    fix data 7

--- components/Home/TopNewsComponent.tsx
+++ components/Home/TopNewsComponent.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const TopNewsComponent: FC<Props> = (props) => {
   const { newsData } = props;
-  const { id, created_at, title } = newsData;
+  const { id_number, created_at, title } = newsData;
   const createdAtString: string = created_at.toString();
   const createdAtDate = createdAtString.slice(0, 10);
   return (
     <div>
-      <Link href={`/news/${id}`}>
+      <Link href={`/news/${id_number}`}>
         <div className="w-80 h-56 bg-white mx-10 px-5 rounded-md py-8 hover:bg-gray-100 hover:animate-scale-up-center">
           <div className="text-md my-3">{createdAtDate}</div>
           <div className="text-xl">{title}</div>
